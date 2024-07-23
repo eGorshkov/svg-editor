c72baa814771d1883e2c83a6ee93e36b75a99819
Evgenii <shkovegor@gmail.com>
2024-07-23 22:02:27 +0300

    Поворот/перемещение редактора (#23)
    * Поворот/перемещение редактора
    
    * Добавлен зум редактора
    
    ---------
    
    Co-authored-by: Evgeniy Gorshkov <gorshkov.e.s@effective-group.ru>

--- src/components/core/editor.js
+++ src/components/core/editor.js
@@ -10,15 +10,19 @@ export class Editor extends Core {
   __type = 'editor';
   #EDITOR_TEMPLATE_ID = 'editor-template';
   #config = null;
-
+   
   onChange = new Subject(null, false);
 
   get configuration() {
     return {
+      config: this.#config.config,
       items: this.items,
       layers: this.items.map(layer => layer.getConfiguration()),
       toJson() {
-        return JSON.stringify(this.layers);
+        return JSON.stringify({
+                    config: this.config,
+                    layers: this.layers
+                });
       }
     };
   }
@@ -26,12 +30,13 @@ export class Editor extends Core {
   constructor(config) {
     super('svg');
     this.template.setAttribute('id', this.#EDITOR_TEMPLATE_ID);
-    this.#config = config;
+    this.#config = {config: {}, ...config};
   }
 
   init(config) {
     this.#setListener();
     this.#initObserver();
+    this.#initStyles();
     if (this.#config?.layers?.length) this.load(this.#config?.layers.sort((a, b) => (a.order - b.order ? 1 : -1)));
 
     this.shapes.filter(shape => shape.type === "link").forEach(link => globalThis.LINK.set.next([link.type, link]));
@@ -77,6 +82,16 @@ export class Editor extends Core {
     );
 
     document.addEventListener(
+        'mousewheel',
+        (evt) => {
+            if (!globalThis.ACTIVE_ITEM_SUBJECT.getValue()) {
+                this.#setStyle('zoom', evt.deltaY * -0.01, 1);
+                this.#initStyles();
+            }
+        }
+    )
+
+    document.addEventListener(
       'keydown',
       evt => {
         const active = globalThis.ACTIVE_ITEM_SUBJECT.getValue();
@@ -91,7 +106,28 @@ export class Editor extends Core {
             default:
               break;
           }
-        }
+        } else {
+          switch (evt.key) {
+            case 'ArrowUp':
+                this.#setStyle('translateY', -10)
+                this.#initStyles();
+                break;
+            case 'ArrowLeft':
+                evt.shiftKey ? this.#setStyle('rotate', -.1) : this.#setStyle('translateX', -10);
+                this.#initStyles();
+                break;
+            case 'ArrowDown':
+                this.#setStyle('translateY', 10)
+                this.#initStyles();
+                break;
+            case 'ArrowRight':
+                evt.shiftKey ? this.#setStyle('rotate', .1) : this.#setStyle('translateX', 10);
+                this.#initStyles();
+                break;
+            default:
+              break;
+            }
+                }
       },
       true
     );
@@ -120,5 +156,32 @@ export class Editor extends Core {
       if (added.length || removed.length) this.onChange.next({ added, removed });
     });
     observer.observe(this.template, { subtree: true, childList: true });
+  }
+
+  #initStyles() {
+    let transform = "";
+    let zoom = 1;
+    Object.entries(this.#config.config).forEach(([key, value]) => {
+      console.log(key, value)
+      switch (key) {
+        case 'zoom':
+            zoom = value;
+            break;
+        case 'rotate':
+          transform += value ? `${key}(calc(${value} * 3.142rad)) ` : ""
+          break;
+        case 'translateX':
+        case 'translateY':
+          transform += value ? `${key}(${value}px) ` : ""
+        default:
+          break;
+      }
+    })
+    this.template.style.transform = transform.trim();
+    this.template.style.zoom = zoom;
+  }
+
+  #setStyle(key, value, def = 0) {
+    this.#config.config[key] = (this.#config.config[key]??def) + value;
   }
 }
