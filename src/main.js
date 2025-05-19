a4aa64498e884b199aeb617dd140791ae1a4bb44
Evgenii <shkovegor@gmail.com>
2025-05-19 20:05:03 +0300

    Update main.js (#25)

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import compose from './components/helpers/compose.js';
 import { MOCK_DEFAULT_LAYERS } from './mock/mock-default-layers.js';
 import { createCommon, createEditor, createTemplates, createUI } from './creator.js';
 
+console.log("Start")
 compose(createCommon, createEditor, createTemplates, createUI)(MOCK_DEFAULT_LAYERS);
